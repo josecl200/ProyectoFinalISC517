7ab3ad2d02ad314ad445ed44569c53b9a525dc6c
José C. Ureña <20160138@ce.pucmm.edu.do>
2020-08-22 15:37:30 -0400

    Balanceador y enrutador con Zuul+Ribbon

---
+++ gatewaybalanceador/src/test/java/edu/pucmm/josecl200/finalavanzada/gatewaybalanceador/GatewaybalanceadorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pucmm.josecl200.finalavanzada.gatewaybalanceador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewaybalanceadorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
